fa03fb1253bf9ee552f15c299e7c2a2912dc6d7a
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2024-06-19 11:06:15 +0700

    Update main.cpp

--- Olympic Sinh Viên/2020/Chuyên tin/SƠN PHẢN QUANG/main.cpp
+++ Olympic Sinh Viên/2020/Chuyên tin/SƠN PHẢN QUANG/main.cpp
@@ -1,83 +1,24 @@
-// https://github.com/SPyofgame200/Editorial/blob/master/Olympic%20Sinh%20Vi%C3%AAn%202020%20-%20Kh%C3%B4ng%20chuy%C3%AAn%20-%20Ph%C3%A2n%20s%E1%BB%91.md
+// https://github.com/SPyofgame200/Editorial/blob/master/Olympic%20Sinh%20Vi%C3%AAn%202020%20-%20Chuy%C3%AAn%20tin%20-%20S%C6%A1n%20ph%E1%BA%A3n%20quang.md
 
 #include <iostream>
-#include <cstring>
-#include <vector>
 
 using namespace std;
 
-const int LIM = 1e6 + 1;
+typedef long long ll;
 
-/// SPyofgame linear sieving
-vector<int> prime; /// prime list
-vector<int> lpf; /// lowest prime factor
-void sieve(int n = LIM)
+ll f(ll n)
 {
-    prime.assign(1, 2);
-    lpf.assign(n + 1, 2);
+    ll res = 0;
+    for (ll x = 2; x <= n; x *= 2)
+        res += n / x;
 
-    lpf[1] = -2;
-    for (int x = 3; x <= n; x += 2)
-    {
-        if (lpf[x] == 2) prime.push_back(lpf[x] = x);
-        for (int i = 0; i < prime.size() && prime[i] <= lpf[x] && prime[i] * x <= n; ++i)
-            lpf[prime[i] * x] = prime[i];
-    }
-}
-
-int G[LIM];
-int query()
-{
-    int n;
-    cin >> n;
-
-    bool ok = false;
-    memset(G, 0, sizeof(G));
-    for (int i = 1; i <= 2 * n; ++i)
-    {
-        int x;
-        cin >> x;
-
-        if (ok) continue;
-        while (x > 1)
-        {
-            int p = lpf[x], f = 0;
-            do x /= p, ++f;
-            while (lpf[x] == p);
-            
-            if (p == 2 || p == 5) continue;
-            if (i <= n)
-                G[p] -= f;
-            else 
-            {
-                G[p] += f;
-                if (G[p] > 0) /// f[i] - d[i] > 0
-                {
-                    ok = true;
-                    break;
-                }
-            }
-        }
-    }
-
-    cout << (ok ? "repeating\n" : "finite\n");
-    return 0;
+    return res;
 }
 
 int main()
 {
-    ios::sync_with_stdio(NULL);
-    cin.tie(NULL);
-
-    int q;
-    cin >> q;
-
-    sieve();
-    while (q-->0)
-    {
-        query();
-
-    }
-    
+    ll l, r;
+    cin >> l >> r;
+    cout << f(r) - f(l - 1);
     return 0;
 }
